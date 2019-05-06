2fe13d10a7bb0f141204ed005ca9c18d43fc9801
Amy Goldlist <amygoldlist@gmail.com>
2019-05-05 19:54:30 -0700

    playing wiht R

--- Scripts/sandbox/reading_docs.R
+++ Scripts/sandbox/reading_docs.R
@@ -1,3 +1,9 @@
+###########
+##
+##  This is a very specific script to extract info from my mom's collection of Word docs.
+##  Luckily, my mom is super organized, so extrcacting the data is not too terrible
+
+
 
 ### open the correct library
 library(antiword)
@@ -8,13 +14,23 @@ library(tidyverse)
 
 pancakes <- antiword("Pancakes.doc")
 
-pancakes
+#pancakes
 
+split_cakes <- str_split(pancakes, "\r\n")[[1]]
 
+##split_cakes[lapply(split_cakes,length)>0] 
 
-#############
-##What happens if I use docx?  Is it easier?
+##split_cakes
 
-library(xml2)
+##str_locate(split_cakes[[1]],"Ingredients")
 
-#amy <- xml2::read_xml("Pancakes.doc")
+#?str_locate
+
+###  This returns the number of WHERE Ingredients are
+ingredient_index <- which(!is.na(str_locate(split_cakes,"Ingredients")[,1]))
+
+###Next comes method
+method_index <- which(!is.na(str_locate(split_cakes,"Method")[,1]))
+
+i <- ingredient_index
+#Ingredient_list <- while ()
